671de27810f695a440f5b8fb239fc66068840031
GamerCleanVic <jonassd2000@gmail.com>
2024-05-20 17:12:55 -0300

    import + export

---
+++ src/components/Geo.tsx
@@ -0,0 +1,25 @@
+export const Square = () =>{
+    return(
+        <div className="
+            w-52
+            h-52
+            bg-purple-800
+            text-white
+        ">
+            Texto qualquer inspirador.
+        </div>
+    );
+}
+export const Circle = () =>{
+    return(
+        <div className="
+            w-52
+            h-52
+            bg-purple-800
+            text-white
+            rounded-full
+        ">
+            Texto qualquer inspirador.
+        </div>
+    );
+}
