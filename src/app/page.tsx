9608e102182f330db07f19f11a59c057dd82cf2f
GamerCleanVic <jonassd2000@gmail.com>
2024-10-23 18:27:30 -0300

    props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,16 @@ const Page = () =>{
       <h1 className="font-bold text-2xl">Olá mundo!</h1>
       <h2>Algum outro texto</h2>
 
-      <Person />
+      <Person
+        name = 'Elon Musk'
+        avatar = 'https://classic.exame.com/wp-content/uploads/2021/04/Elon-Musk.jpg'
+        roles = {['CEO da Tesla', 'CEO da SpaceX']}
+      />
+      <Person
+        name = 'Jeff Bezos'
+        avatar = 'https://investidorsardinha.r7.com/wp-content/uploads/2020/09/jeff-bezos-a-historia-do-homem-mais-rico-do-mundo-1-scaled.jpg'
+        roles = {['CEO da Amazon', 'CEO da Twitch']}
+      />
     </div>
   );
 }
--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -2,14 +2,14 @@ const weekDay = (today: Date) => {
     return new Intl.DateTimeFormat('pt-BR', {weekday: 'long' }).format(new Date());
 }
 
-export const Person = () =>{
-    const today: Date = new Date();
-    const data = {
-        name: 'Elon Musk',
-        avatar: 'https://classic.exame.com/wp-content/uploads/2021/04/Elon-Musk.jpg',
-        roles: ['CEO da Tesla', 'CEO da SpaceX']
-    }
+type Props = {
+    name: string;
+    avatar: string;
+    roles: string[];
+    adress?: string;
+}
 
+export const Person = ({name, avatar, roles}: Props) =>{
     return(
         <>
             <div
@@ -21,17 +21,17 @@ export const Person = () =>{
                     p-3
                 "
             >
-            <h1>{data.name} - {weekDay(today)}</h1>
+            <h1>{name}</h1>
                 <img
-                    src={data.avatar}
-                    alt={data.name}
+                    src={avatar}
+                    alt={name}
                     className="
                         w-40
                     "
                 />
                 <ul>
-                    <li>{data.roles[0]}</li>
-                    <li>{data.roles[1]}</li>
+                    <li>{roles[0]}</li>
+                    <li>{roles[1]}</li>
                 </ul>
             </div>
         </>
