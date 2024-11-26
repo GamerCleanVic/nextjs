9e2b0911d43c72a92131285023a0d4bd9089dcd0
GamerCleanVic <jonassd2000@gmail.com>
2024-11-26 17:30:58 -0300

    ?? operator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
-//Modulo 2: Children do componente #8
+//Modulo 2: Operador lógico && #10
 import { Card } from '@/components/Card';
-import { Circle } from '@/components/Circle';
 
 const Page = () =>{
   return (
@@ -8,13 +7,10 @@ const Page = () =>{
       <h1 className="font-bold text-2xl">Olá mundo!</h1>
       <h2>Algum outro texto</h2>
 
-      <Card>
-        <>
-          <h3 className='text-3xl font-bold italic'>"Frase de efeito"</h3>
-          <p className='text-right text-sm'>- Autor desconhecido</p>
-          <Circle />
-        </>
-      </Card>
+      <Card
+		phrase="Alguma frase TOP"
+		author="Coach de milhões"
+      />
     </div>
   );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,22 +1,25 @@
 import { ReactNode } from "react";
 
 type Props = {
-    children: ReactNode;
+    phrase: string;
+    author?: string;
 }
-export const Card = ({ children }: Props) => {
-    return(
-        <div className="
-            border
-            border-green-500
-            rounded-sm
-            p-4
-            m-5
-            text-2xl
-            text-center
-            italic
-            w-96
-        ">
-            {children}
-        </div>
-    );
+export const Card = ({ phrase, author }: Props) => {
+	return(
+			<div className="
+							border
+							border-green-500
+							rounded-sm
+							p-4
+							m-5
+							text-2xl
+							text-center
+							italic
+							w-96
+			">
+				<h3 className='text-3xl font-bold italic'>"{phrase}"</h3>
+				<p className='text-right text-sm'>- {author ?? "Autor Desconhecido"}</p>
+
+			</div>
+	);
 }
