1d898ee150fb2d0d2e655b364970a60bc2f1ea02
GamerCleanVic <jonassd2000@gmail.com>
2024-12-02 17:33:46 -0300

    renderizar listas - B7Web

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,21 @@
-//Modulo 2: Renderizando Listas #11
-import { Card } from '@/components/Card';
+//Modulo 2: Filtrando Listas #12
+import { peopleList } from '@/data/peopleList' ;
 
 const Page = () =>{
   return (
     <div>
       <h1 className="font-bold text-2xl">Olá mundo!</h1>
-      <h2>Algum outro texto</h2>
-
-      <Card
-        phrase="Alguma frase TOP"
-        // author="Coach de milhões"
-      />
+      <h2 className='mb-4'>Algum outro texto</h2>
+      {peopleList.length > 0 &&
+        <ul className='m-5'>
+          {
+            peopleList.map(person =>
+              <li key={person.id} className='list-disc text-purple-600 hover:text-yellow-400 hover:transition-colors hover:duration-900'>
+                <b className='hover:text-green-400'>{person.name} - <code className='hover:text-yellow-400 hover:border-4 hover:border-purple-600 hover:p-1 hover:mb-4'>{person.profession}</code></b>
+              </li>
+          )}
+        </ul>
+      }
     </div>
   );
 }
